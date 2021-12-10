edec23b6684398da8fc88ee9dfac4b48114f61bf
Rizky Dwi Saputra <rizkydwisaputrar1@gmail.com>
2021-12-10 07:28:32 -0800

    init

--- fragment/app/src/main/java/com/rizkydwisaputra/pindahfragment/FragmentSatu.kt
+++ fragment/app/src/main/java/com/rizkydwisaputra/pindahfragment/FragmentSatu.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.fragment_satu.*
 
-
 /**
  * A simple [Fragment] subclass.
  * Use the [FragmentSatu.newInstance] factory method to
--- fragment/app/src/main/java/com/rizkydwisaputra/pindahfragment/MainActivity.kt
+++ fragment/app/src/main/java/com/rizkydwisaputra/pindahfragment/MainActivity.kt
@@ -2,13 +2,14 @@ package com.rizkydwisaputra.pindahfragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val fragmentSatu = FragmentSatu()
-        val fragment= supportFragmentManager.findFragmentByTag(FragmentSatu::class.java.simpleName)
+        val fragment: Fragment?=supportFragmentManager.findFragmentByTag(FragmentSatu::class.java.simpleName)
         if(fragment !is FragmentSatu){
             supportFragmentManager.beginTransaction()
                 .add(R.id.container,fragmentSatu, FragmentSatu::class.java.simpleName)
