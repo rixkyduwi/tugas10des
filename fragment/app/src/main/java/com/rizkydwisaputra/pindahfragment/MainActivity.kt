aed3eb4ec0758310bf23414705b471dbfe71d745
Rizky Dwi Saputra <rizkydwisaputrar1@gmail.com>
2021-12-10 06:01:34 -0800

    Initial commit

---
+++ fragment/app/src/main/java/com/rizkydwisaputra/pindahfragment/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.rizkydwisaputra.pindahfragment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val fragmentSatu = FragmentSatu()
+        val fragment= supportFragmentManager.findFragmentByTag(FragmentSatu::class.java.simpleName)
+        if(fragment !is FragmentSatu){
+            supportFragmentManager.beginTransaction()
+                .add(R.id.container,fragmentSatu, FragmentSatu::class.java.simpleName)
+                .commit()
+        }
+    }
+}
